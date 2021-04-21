a57877422f2e648afca8d267f2c4f6486a9b27fd
Jake <ljangwon@gmail.com>
2021-04-22 00:08:10 +0900

    bootstrap

---
+++ application/logs/log-2021-04-21.php
@@ -0,0 +1,26 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2021-04-21 17:00:39 --> Config Class Initialized
+DEBUG - 2021-04-21 17:00:39 --> Hooks Class Initialized
+DEBUG - 2021-04-21 17:00:39 --> Utf8 Class Initialized
+DEBUG - 2021-04-21 17:00:39 --> UTF-8 Support Enabled
+DEBUG - 2021-04-21 17:00:39 --> URI Class Initialized
+DEBUG - 2021-04-21 17:00:39 --> Router Class Initialized
+DEBUG - 2021-04-21 17:00:39 --> Output Class Initialized
+DEBUG - 2021-04-21 17:00:39 --> Security Class Initialized
+DEBUG - 2021-04-21 17:00:39 --> Input Class Initialized
+DEBUG - 2021-04-21 17:00:39 --> Global POST and COOKIE data sanitized
+DEBUG - 2021-04-21 17:00:39 --> Language Class Initialized
+DEBUG - 2021-04-21 17:00:39 --> Loader Class Initialized
+DEBUG - 2021-04-21 17:00:39 --> Helper loaded: url_helper
+DEBUG - 2021-04-21 17:00:39 --> Helper loaded: alert_helper
+DEBUG - 2021-04-21 17:00:39 --> Session Class Initialized
+DEBUG - 2021-04-21 17:00:39 --> Helper loaded: string_helper
+DEBUG - 2021-04-21 17:00:39 --> Database Driver Class Initialized
+ERROR - 2021-04-21 17:00:39 --> Severity: 8192  --> mysql_pconnect(): The mysql extension is deprecated and will be removed in the future: use mysqli or PDO instead D:\dev\bin\xampp\htdocs\leanmath\system\database\drivers\mysql\mysql_driver.php 91
+DEBUG - 2021-04-21 17:00:39 --> A session cookie was not found.
+DEBUG - 2021-04-21 17:00:39 --> Session routines successfully run
+DEBUG - 2021-04-21 17:00:39 --> Controller Class Initialized
+DEBUG - 2021-04-21 17:00:39 --> File loaded: application/views/bootstrap_v.php
+DEBUG - 2021-04-21 17:00:39 --> Final output sent to browser
+DEBUG - 2021-04-21 17:00:39 --> Total execution time: 0.1061
