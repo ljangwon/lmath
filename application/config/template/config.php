6500c6cb7416b6fd91e6d0f98fdc93103611046b
Lean Edu <ljangwon@gmail.com>
2021-06-01 23:08:06 +0900

    Update config.php

--- application/config/template/config.php
+++ application/config/template/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'aw2q132141545123^%12an?@kfklsfs';
 
 /*
 |--------------------------------------------------------------------------
